793f42203eaa1c05423c8b6ba5d06ef71d39d5e0
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-02 13:48:32 -0500

    Defined a create method that calls the ImdbService class method to get the plot of the movie from the api call.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+    require 'uri'
 
     def index
         @movies = Movie.all 
@@ -7,4 +8,25 @@ class MoviesController < ApplicationController
     def show
         @movie = Movie.find(params[:id])
     end
+
+    def create
+        @movie = Movie.new(movie_params)
+
+        hash = ImdbService.new 
+        escaped_title = URI.escape(@movie.title)
+        @movie.plot = hash.get_plot_by_title(escaped_title)
+
+        if @movie.save 
+            redirect_to @movie
+        else 
+            render "new"
+        end
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title, :release_date, :plot)
+    end
+
 end
