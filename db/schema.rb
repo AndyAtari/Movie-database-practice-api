82c803bbb504400c719bb0d9e8601452c341821b
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-02 13:53:28 -0500

    Created migration of adding plot column to movies table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_205022) do
+ActiveRecord::Schema.define(version: 2021_04_02_185058) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.date "release_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "plot"
   end
 
 end
