88e54abd04af7abe76ade7e0853b7114dce0658c
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-02 16:18:20 -0500

    Added form for creating new movie.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,10 @@ class MoviesController < ApplicationController
         @movie = Movie.find(params[:id])
     end
 
+    def new
+        @movie = Movie.new 
+    end
+
     def create
         @movie = Movie.new(movie_params)
 
